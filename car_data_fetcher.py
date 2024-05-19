6c4bdb47486cec1d92ecd83d8ece3b22127d6f0d
NoIdeaForMyName <maksanty.michal@gmail.com>
2024-05-19 15:27:45 +0200

    PEP8 compliance ensured in car parser files and also all pip install commands added in project file

--- car_data_fetcher.py
+++ car_data_fetcher.py
@@ -1,5 +1,4 @@
 from typing import Any
-from matplotlib.pylab import det
 import requests
 import bs4
 from bs4 import BeautifulSoup
@@ -7,74 +6,87 @@ import csv
 import time
 import os
 
-from tqdm import tqdm
-
 from car_parsing import CarParser, OLX_CarParser, OTOMOTO_CarParser, parseStatus
 
 
 def get_html_content(url: str, retries=3):
+    """
+    tries to connect to the given url and returns html content from the website
+    """
     if retries < 0:
         return None
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    headers = {  # without it, server may recognize this scraper as a bot and won't share resources
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     }
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()  # Check for any errors in the HTTP response
         if len(response.text) < 10000:
-            print('Response length was too small')
+            print("Response length was too small")
             print(url)
-            return get_html_content(url, retries-1)
-        return response     
+            return get_html_content(url, retries - 1)
+        return response
     except Exception as e:
         print(e)
-        time.sleep(2)
-        return get_html_content(url, retries-1)
+        time.sleep(
+            2
+        )  # sometimes error is caused by the scraper running too fast - the server is blocking resources
+        return get_html_content(url, retries - 1)
 
 
 def get_car_href_list(url):
+    """
+    returns list of all urls with car offers
+    """
     response = get_html_content(url, retries=50)
     if response is None:
-        raise requests.exceptions.RequestException('An error during fetching has occured')
-    soup = BeautifulSoup(response.text, 'html.parser')
+        raise requests.exceptions.RequestException(
+            "An error during fetching has occured"
+        )
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    car_container = soup.find('div', attrs={'data-testid': 'search-results'})
+    car_container = soup.find("div", attrs={"data-testid": "search-results"})
     if not isinstance(car_container, bs4.Tag):
-        #raise requests.exceptions.RequestException('Site does not have demanded element: {data-testid: search-results}')
-        print('Site does not have demanded element: {data-testid: search-results}:', url)
-        print('soup len:', len(soup.text))
-        print('response len:', len(response.text))
+        print(
+            "Site does not have demanded element: {data-testid: search-results}:", url
+        )
+        print("soup len:", len(soup.text))
+        print("response len:", len(response.text))
         return []
-    car_list = car_container.find_all('article', class_='ooa-yca59n e1i3khom0') 
-    car_href_list = [article.find('a')['href'] for article in car_list]
+    car_list = car_container.find_all("article", class_="ooa-yca59n e1i3khom0")
+    car_href_list = [article.find("a")["href"] for article in car_list]
 
-    #car_href_list = [a['href'] for a in soup.find_all('a')]
-    #car_href_list = [a.text for a in soup.find_all('a')]
-     
     return car_href_list
 
 
-def get_cars_to_parse(car_urls: list[str]) -> list[CarParser]:
-    get_cars_to_parse.failed = 0
+def get_cars_to_parse(car_urls: list[str]) -> tuple[list[CarParser], int]:
+    """
+    from list of urls with car offers, it creates and returns a list of CarParser objects
+    that contain all informations about the cars from the website
+    """
+    failed = 0
     parsed_car_list: list[CarParser] = []
     url_counter = 0
     for url in car_urls:
         html = get_html_content(url)
-        if html is None: # unable to fetch data about this specific car
-            get_cars_to_parse.failed += 1
+        if html is None:  # unable to fetch data about this specific car
+            failed += 1
             continue
         parsed_car_list.append(OTOMOTO_CarParser(html.text))
         url_counter += 1
-        #print(f'{round(url_counter*100/url_number, 2)}%', end='; ', flush=True)
-    #print()
-    return parsed_car_list
+    return parsed_car_list, failed
 
 
-#def parse_cars(cars_list: list[CarParser]) -> dict[parseStatus, list[CarParser]]:
 def parse_cars(cars_list: list[CarParser]) -> int:
-    for car in cars_list: car.parse_html()
-    #return {key: list(filter(lambda car: car.parsed == key, cars_list)) for key in parseStatus.__members__.values()}
+    """
+    parses every car provided in the input list
 
+    modifies the input list so that only spccesfully parsed cars stay
+
+    returns number of failed parsing attempt
+    """
+    for car in cars_list:
+        car.parse_html()
     car_nb = len(cars_list)
     for i in reversed(range(len(cars_list))):
         if not cars_list[i].parsed == parseStatus.SUCCESFULLY_PARSED:
@@ -84,7 +96,7 @@ def parse_cars(cars_list: list[CarParser]) -> int:
 
 
 def write_to_csv(detail_dict_list: list[dict[str, str]], filename):
-    with open(filename, 'a', newline='', encoding='utf-8') as csvfile:
+    with open(filename, "a", newline="", encoding="utf-8") as csvfile:
         fieldnames = detail_dict_list[0].keys()
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         if csvfile.tell() == 0:
@@ -94,72 +106,89 @@ def write_to_csv(detail_dict_list: list[dict[str, str]], filename):
 
 
 def get_all_pagination_urls(first_url: str) -> list[str]:
-    infix = '?page='
+    """
+    from the first page, given as an argument, returns links to the next pages with cars
+    """
+    infix = "?page="
     html = get_html_content(first_url)
     if html is None:
-        raise requests.exceptions.RequestException('An error during initial fetching has occured')
-    soup = BeautifulSoup(html.text, 'html.parser')
-    paginations = soup.find('ul', class_='pagination-list')
+        raise requests.exceptions.RequestException(
+            "An error during initial fetching has occured"
+        )
+    soup = BeautifulSoup(html.text, "html.parser")
+    paginations = soup.find("ul", class_="pagination-list")
     if isinstance(paginations, bs4.NavigableString) or paginations is None:
-        raise requests.exceptions.RequestException('An error during initial fetching has occured')
-    last_url_number = int(paginations.find_all('li', attrs={'data-testid':'pagination-list-item'})[-1].text)
+        raise requests.exceptions.RequestException(
+            "An error during initial fetching has occured"
+        )
+    last_url_number = int(
+        paginations.find_all("li", attrs={"data-testid": "pagination-list-item"})[
+            -1
+        ].text
+    )
 
-    return [first_url + infix + str(counter) for counter in range(1, last_url_number+1)]
+    return [
+        first_url + infix + str(counter) for counter in range(1, last_url_number + 1)
+    ]
 
 
 def main():
-    #first_url = 'https://www.olx.pl/motoryzacja/samochody/volkswagen/?search%5Bfilter_enum_model%5D%5B0%5D=golf'
-    #first_url = 'https://www.olx.pl/motoryzacja/samochody/volkswagen/q-golf-4/?search%5Bfilter_enum_model%5D%5B0%5D=golf'
 
     car_type_pagination_url_dict = {
-        ('Volkswagen', 'Golf'): 'https://www.otomoto.pl/osobowe/volkswagen/golf',
-        ('BMW', 'Seria 3'): 'https://www.otomoto.pl/osobowe/bmw/seria-3',
-        ('Opel', 'Corsa'): 'https://www.otomoto.pl/osobowe/opel/corsa'
+        ("Volkswagen", "Golf"): "https://www.otomoto.pl/osobowe/volkswagen/golf",
+        ("BMW", "Seria 3"): "https://www.otomoto.pl/osobowe/bmw/seria-3",
+        ("Opel", "Corsa"): "https://www.otomoto.pl/osobowe/opel/corsa",
     }
 
-    # minimum pagination:
-    pages_per_car = min([len(get_all_pagination_urls(url)) for url in car_type_pagination_url_dict.values()])
-    cars_per_page = len(get_car_href_list(list(car_type_pagination_url_dict.values())[0]))
+    # minimum pagination: - so that for every car type there will be approx. the same number of samples
+
+    pages_per_car = min(
+        [
+            len(get_all_pagination_urls(url))
+            for url in car_type_pagination_url_dict.values()
+        ]
+    )
+    cars_per_page = len(
+        get_car_href_list(list(car_type_pagination_url_dict.values())[0])
+    )
     car_nb_per_model = pages_per_car * cars_per_page
 
     sum_failed = 0
     for (brand, model), first_url in car_type_pagination_url_dict.items():
-        print(f'Fetching data for {brand} {model}')
-        csv_filename = f'{brand}_{model}.csv'
+        print(f"Fetching data for {brand} {model}")
+        csv_filename = f"{brand}_{model}.csv"
         urls = get_all_pagination_urls(first_url)[:pages_per_car]
 
         car_pages_nb = 0
         pagination_sites_car_urls: list[list[str]] = []
         for url in urls:
-            print('Done:', round(car_pages_nb*100/car_nb_per_model, 2), '%')
+            print("Done:", round(car_pages_nb * 100 / car_nb_per_model, 2), "%")
             car_list = get_car_href_list(url)
             car_pages_nb += len(car_list)
             if car_list != []:
                 pagination_sites_car_urls.append(car_list)
             if car_pages_nb >= car_nb_per_model:
-                diff = car_pages_nb-car_nb_per_model
+                diff = car_pages_nb - car_nb_per_model
                 lst_len = len(pagination_sites_car_urls[-1])
-                del pagination_sites_car_urls[-1][lst_len-diff:]
+                del pagination_sites_car_urls[-1][lst_len - diff :]
                 break
-
         print()
-        [print('car list:', len(car)) for car in pagination_sites_car_urls]
+        [print("car list:", len(car)) for car in pagination_sites_car_urls]
 
         if os.path.exists(csv_filename):
-            print('Overwriting', csv_filename)
+            print("Overwriting", csv_filename)
             os.remove(csv_filename)
-
-        #for url in tqdm(urls, desc='Every website'):
         loop_size = len(pagination_sites_car_urls)
         counter = 0
         for car_urls in pagination_sites_car_urls:
-            print('Left:', round(counter*100/loop_size, 2), '%')
+            print("Left:", round(counter * 100 / loop_size, 2), "%")
             counter += 1
-            cars = get_cars_to_parse(car_urls)
-            sum_failed += get_cars_to_parse.failed
+            cars, failed = get_cars_to_parse(car_urls)
+            sum_failed += failed
             sum_failed += parse_cars(cars)
             write_to_csv([car.details for car in cars], csv_filename)
-    print(f'Overall failed: {sum_failed}')
+    print(f"Overall failed: {sum_failed}")
+
 
 if __name__ == "__main__":
     main()
--- car_parsing.py
+++ car_parsing.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 from enum import Enum, auto
 import re
 
-from matplotlib.pylab import det
 
 class parseStatus(Enum):
     UNPARSED = auto()
@@ -14,28 +13,29 @@ class parseStatus(Enum):
 
 class CarParser(metaclass=abc.ABCMeta):
     def __init__(self, html) -> None:
-        #self.html: str = html
-        self.soup = BeautifulSoup(html, 'html.parser')
+        # self.html: str = html
+
+        self.soup = BeautifulSoup(html, "html.parser")
         self.parsed: parseStatus = parseStatus.UNPARSED
 
         self.details = {
-            'brand': 'Unknown',
-            'model': 'Unknown',
-            'generation': 'Unknown',
-            'eng_cap': 'Unknown', # in cm^3
-            'prod_year': 'Unknown',
-            'power': 'Unknown', # KM
-            'fuel_type': 'Unknown', # gasoline | diesel | lpg
-            'car_body': 'Unknown', # combi | hatchback | convertible etc.
-            'mileage': 'Unknown', # km
-            'color': 'Unknown',
-            'condition': 'Unknown', # damaged | undamaged
-            'transmission': 'Unknown', # manual | automatic
-            'origin': 'Unknown', # poland | germany etc.
-            'price': 'Unknown',
-            'source': 'Unknown'
+            "brand": "Unknown",
+            "model": "Unknown",
+            "generation": "Unknown",
+            "eng_cap": "Unknown",  # in cm^3
+            "prod_year": "Unknown",
+            "power": "Unknown",  # KM
+            "fuel_type": "Unknown",  # gasoline | diesel | lpg
+            "car_body": "Unknown",  # combi | hatchback | convertible etc.
+            "mileage": "Unknown",  # km
+            "color": "Unknown",
+            "condition": "Unknown",  # new | used
+            "transmission": "Unknown",  # manual | automatic
+            "origin": "Unknown",  # poland | germany etc.
+            "price": "Unknown",
+            "source": "Unknown",
         }
-    
+
     @abc.abstractmethod
     def parse_html(self) -> parseStatus:
         pass
@@ -46,111 +46,115 @@ class CarParser(metaclass=abc.ABCMeta):
 
 
 class OLX_CarParser(CarParser):
-    '''
+    """
     OLX parser does not support 'brand' detail, so it can be passed in constructor
-    '''
+    """
 
     translation = {
-        'Model': 'model',
-        'Poj. silnika': 'eng_cap',
-        'Rok produkcji': 'prod_year',
-        'Moc silnika': 'power',
-        'Paliwo': 'fuel_type',
-        'Typ nadwozia': 'car_body',
-        'Przebieg': 'mileage',
-        'Kolor': 'color',
-        'Stan techniczny': 'condition',
-        'Skrzynia biegów': 'transmission',
-        'Kraj pochodzenia': 'origin',
+        "Model": "model",
+        "Poj. silnika": "eng_cap",
+        "Rok produkcji": "prod_year",
+        "Moc silnika": "power",
+        "Paliwo": "fuel_type",
+        "Typ nadwozia": "car_body",
+        "Przebieg": "mileage",
+        "Kolor": "color",
+        "Stan techniczny": "condition",
+        "Skrzynia biegów": "transmission",
+        "Kraj pochodzenia": "origin",
     }
 
-    def __init__(self, html, brand:str='Unknown') -> None:
+    def __init__(self, html, brand: str = "Unknown") -> None:
         super().__init__(html)
-        self.details['brand'] = brand
-        self.details['source'] = 'OLX'
+        self.details["brand"] = brand
+        self.details["source"] = "OLX"
 
     def parse_html(self) -> parseStatus:
-        car_data = self.soup.find('ul', class_='css-sfcl1s')
+        car_data = self.soup.find("ul", class_="css-sfcl1s")
         if car_data is None or isinstance(car_data, bs4.NavigableString):
             self.parsed = parseStatus.UNSUCCESFULLY_PARSED
             return self.parsed
-        details_list: list[str] = [t.text for t in car_data.find_all('li', class_='css-1r0si1e')][1:]
+        details_list: list[str] = [
+            t.text for t in car_data.find_all("li", class_="css-1r0si1e")
+        ][1:]
         self.fill_details(details_list)
         self.parsed = parseStatus.SUCCESFULLY_PARSED
         return self.parsed
-    
 
     def fill_details(self, det):
-        name_detail_list = [[d_.strip() for d_ in d.split(':')] for d in det]
+        name_detail_list = [[d_.strip() for d_ in d.split(":")] for d in det]
         for name, detail in name_detail_list:
             key = OLX_CarParser.translation.get(name, None)
             if key:
                 self.details[OLX_CarParser.translation[name]] = detail
-        self.details['price'] = self.get_price()
+        self.details["price"] = self.get_price()
         gen = self.get_generation()
         if gen is not None:
-            self.details['generation'] = gen
+            self.details["generation"] = gen
 
     def get_price(self) -> str:
-        price_tag = self.soup.find('div', attrs={'data-testid': 'ad-price-container'})
+        price_tag = self.soup.find("div", attrs={"data-testid": "ad-price-container"})
         if isinstance(price_tag, bs4.NavigableString) or price_tag is None:
-            return ''
+            return ""
         return price_tag.text
-    
+
     def get_generation(self) -> str | None:
-        model = self.details['model']
-        if model == 'Unknown':
+        model = self.details["model"]
+        if model == "Unknown":
             return None
-        title_tag = self.soup.find('title')
+        title_tag = self.soup.find("title")
         if not isinstance(title_tag, bs4.Tag):
             return None
         title_content = title_tag.text.split()
         for i in range(len(title_content)):
-            if title_content[i] == model and i != len(title_content)-1:
-                return title_content[i+1]
+            if title_content[i] == model and i != len(title_content) - 1:
+                return title_content[i + 1]
         return None
 
 
-'''
-<div data-testid="ad-price-container" class="css-e2ir3r"><h3 class="css-12vqlj3">9 999 zł</h3></div>
-'''
-
-
 class OTOMOTO_CarParser(CarParser):
-    
+
     translation = {
-        'Marka pojazdu': 'brand',
-        'Model pojazdu': 'model',
-        'Generacja': 'generation',
-        'Pojemność skokowa': 'eng_cap',
-        'Rok produkcji': 'prod_year',
-        'Moc': 'power',
-        'Rodzaj paliwa': 'fuel_type',
-        'Typ nadwozia': 'car_body',
-        'Przebieg': 'mileage',
-        'Kolor': 'color',
-        'Stan': 'condition',
-        'Skrzynia biegów': 'transmission',
-        'Kraj pochodzenia': 'origin',
+        "Marka pojazdu": "brand",
+        "Model pojazdu": "model",
+        "Generacja": "generation",
+        "Pojemność skokowa": "eng_cap",
+        "Rok produkcji": "prod_year",
+        "Moc": "power",
+        "Rodzaj paliwa": "fuel_type",
+        "Typ nadwozia": "car_body",
+        "Przebieg": "mileage",
+        "Kolor": "color",
+        "Stan": "condition",
+        "Skrzynia biegów": "transmission",
+        "Kraj pochodzenia": "origin",
     }
 
     def __init__(self, html) -> None:
         super().__init__(html)
-        self.details['source'] = 'OTOMOTO'
+        self.details["source"] = "OTOMOTO"
 
     def parse_html(self) -> parseStatus:
-        #car_data = soup.find('div', class_='ooa-1x860b3 e18eslyg2')
-        car_data = self.soup.find('div', attrs={'data-testid': 'content-details-section'})
+        car_data = self.soup.find(
+            "div", attrs={"data-testid": "content-details-section"}
+        )
         if car_data is None or isinstance(car_data, bs4.NavigableString):
             self.parsed = parseStatus.UNSUCCESFULLY_PARSED
             return self.parsed
-        car_data_list = self.soup.find_all('div', attrs={'data-testid': 'advert-details-item'})
-        details_list: list[tuple[str, str]] = [tuple(t.text for t in tag.find_all(re.compile('[p,a]'))) for tag in car_data_list]
-        details_list: list[tuple[str, str]] = [(tuple_[0], 'Unknown') if len(tuple_) != 2 else tuple_ for tuple_ in details_list]
+        car_data_list = self.soup.find_all(
+            "div", attrs={"data-testid": "advert-details-item"}
+        )
+        details_list: list[tuple[str, str]] = [
+            tuple(t.text for t in tag.find_all(re.compile("[p,a]")))
+            for tag in car_data_list
+        ]
+        details_list: list[tuple[str, str]] = [
+            (tuple_[0], "Unknown") if len(tuple_) != 2 else tuple_
+            for tuple_ in details_list
+        ]
         self.fill_details(details_list)
         self.parsed = parseStatus.SUCCESFULLY_PARSED
         return self.parsed
-    
 
     def fill_details(self, det):
         for name, detail in det:
@@ -158,10 +162,14 @@ class OTOMOTO_CarParser(CarParser):
             key = OTOMOTO_CarParser.translation.get(name, None)
             if key:
                 self.details[OTOMOTO_CarParser.translation[name]] = detail
-        self.details['price'] = self.get_price()
+        self.details["price"] = self.get_price()
 
     def get_price(self) -> str:
-        price_tag = self.soup.find('h3', class_='offer-price__number')
-        if isinstance(price_tag, bs4.NavigableString) or price_tag is None or price_tag.parent is None:
-            return ''
+        price_tag = self.soup.find("h3", class_="offer-price__number")
+        if (
+            isinstance(price_tag, bs4.NavigableString)
+            or price_tag is None
+            or price_tag.parent is None
+        ):
+            return ""
         return price_tag.parent.text
