e659e000abe923015365ce80e544626d4b44043b
NoIdeaForMyName <maksanty.michal@gmail.com>
2024-05-19 14:06:12 +0200

    Deleted unnecessary files and changed name of notebook from proj to project

--- currency_test.py
+++
@@ -1,9 +0,0 @@
-from forex_python.converter import CurrencyRates
-import datetime as dt
-
-def main():
-    c = CurrencyRates()
-    c.get_rate('USD', 'PLN')
-
-if __name__ == "__main__":
-    main()
